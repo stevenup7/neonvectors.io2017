79b27e9eb629fdd261918e2a1bba512fdf955ee3
Steven Upritchard <steven@upritchard.co>
2017-07-06 13:06:34 -0400

    changed to timeline function cleaned up labels

--- ui/src/viz/2017-06-22-resume/resume.js
+++ ui/src/viz/2017-06-22-resume/resume.js
@@ -4,129 +4,157 @@
 
   var chart = new D3VizHelper('#canvas', undefined, 520, {
     top: 10,
-    left: 100,
-    bottom: 20,
-    right: 100
+    left: 20,
+    bottom: 50,
+    right: 20
   });
 
   function drawTimeline(jobData, educationData) {
+    // create the date scale
     var scaleX = d3.scaleTime()
           .range([chart.margins.left, chart.availableWidth])
-          .domain([moment('1992-08-05','YYYY-MM-DD')._d, moment()._d]);
+          .domain([moment('1972-08-05','YYYY-MM-DD')._d, moment()._d])
+          .nice();
+
+    // add a axis using scale above
+    chart.canvas.append("g")
+      .attr("transform", "translate(0," + (chart.height - chart.margins.bottom) + ")")
+      .call(d3.axisBottom(scaleX))
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("transform", "rotate(-65)");;
 
 
-    function jobRight (d, i) {
-      return scaleX(d.endDate) - scaleX(d.startDate);
-    }
 
-    function jobY (offSet) {
-      return function (d, i) {
-        return 10 + offSet;
-      };
-    }
-
-    var c = chart.nvcolors10();
-    var textPositions = [];
-    var offsets = [];
-    jobData.jobs.reverse();
-
-    var jobList = chart.canvas.selectAll('.job')
-      .data(jobData.jobs);
-
-    var joblist = jobList.enter()
-      .append('g')
-      .attr('class', 'jobg')
-      .attr('transform', function (d, i){
-        var y =(d.type === 'secondary') ? 200:208;
-        // var y = (i % 2 === 1 ? 197:203);
-        textPositions.push(scaleX(d.startDate));
-        return 'translate(' +  scaleX(d.startDate) + ',' + y + ')';
-      });
-
-    jobList = chart.canvas.selectAll('.jobg')
-      .data(jobData.jobs);
-
-    jobList
-      .append('rect')
-      .attr('x', 0)
-      .attr('y', 5)
-      .attr('class', 'job-bar')
-      .attr('width', jobRight)
-      .attr('height', 10)
-      .attr('fill', (d,i) => { return  c(i) })
-      .attr('stroke', (d,i) => { return  c(i) });
-
-    jobList
-      .append('text')
-      .attr('x', 0)
-      .attr('y1', jobY(0))
-      .attr('class', 'job-name')
-      .text((d) => { return d.title })
-      .attr('transform', (d, i) => {
-        var offset = 25;
-        var midpoint = jobRight(d, i) / 2 + 10;
-        if (midpoint > offset) {
-          offset = midpoint;
-        }
-
-        if (i === 0) {
-          textPositions[i] += 25;
-        } else {
-          do {
-            var dist = textPositions[i] + offset - textPositions[i - 1];
-            console.log(dist);
-            offset ++;
-          } while (dist < 25 && offset < 100);
-          textPositions[i] += offset;
-        }
-        offsets.push(offset);
-        return 'translate(' + offset + ', -15) rotate(-45) ';
-        // return 'translate(20, 40) rotate(45)'
-      });
-
-    jobList
-      .append('line')
-      .attr('x1', (d, i)=> {
-        return jobRight(d, i) / 2;
-      })
-      .attr('x2', (d, i) => {
-        return 20 + offsets[i] - 25;
-      })
-      .attr('y1', (d,i) => {
-        return jobY(-5)(d,i);
-        // return jobY(+5)(d, i);
-
-      })
-      .attr('y2', (d,i) => {
-        return jobY(-25)(d, i);
-        // return jobY(+25)(d, i);
-      })
-      .attr('stroke', (d,i) => { return  c(i) });
-
-
-    var edList = chart.canvas.selectAll('.ed')
-      .data(educationData.education);
-
-    edList.enter()
-      .append('rect')
-      .attr('class', 'ed')
-      .attr('x', (d) => {
-        return scaleX(d.startDate);
-      })
-      .attr('width', (d) => {
+    function buildTypeTimeline (data, elClass, labelFunction, colors, yOffset) {
+      // calculate the right (width) of a job
+      function barRight (d, i) {
         return scaleX(d.endDate) - scaleX(d.startDate);
-      })
-      .attr('y', (d, i)=> {
-        if (d.type === 'secondary') {
-          return 380;
-        } else {
-          return 370;
-        }
-      })
-      .attr('height', 10)
-      .on('mouseover', (d) => {
-        console.log(d.institution, d.subTitles['Dates attended or expected graduation']);
-      });
+      }
+
+      var textPositions = [];
+      var offsets = [];
+
+      var elSelection = chart.canvas.selectAll('.' + elClass)
+            .data(data);
+
+      elSelection.enter()
+        .append('g')
+        .attr('class', elClass + 'g with-labels')
+        .attr('transform', function (d, i){
+          var y =(d.type === 'secondary') ? yOffset:yOffset + 8;
+          // var y = (i % 2 === 1 ? 197:203);
+          textPositions.push(scaleX(d.startDate));
+          return 'translate(' +  scaleX(d.startDate) + ',' + y + ')';
+        });
+
+      elSelection = chart.canvas.selectAll('.' + elClass + 'g')
+        .data(data);
+
+      elSelection
+        .append('rect')
+        .attr('x', 0)
+        .attr('y', 5)
+        .attr('class', elClass + '-bar')
+        .attr('width', barRight)
+        .attr('height', 10)
+        .attr('fill', (d,i) => {
+          return  d3.rgb(colors(i)).brighter(.5);
+        })
+        .attr('stroke', (d,i) => { return  colors(i) });
+
+      elSelection
+        .append('text')
+        .attr('x', 0)
+        .attr('y1', 10)
+        .attr('class', elClass + '-name is-hideable')
+        .text((d, i) => { return labelFunction(d, i); })
+        .attr('transform', (d, i) => {
+          var offset = 25;
+          var midpoint = barRight(d, i) / 2 + 10;
+          if (midpoint > offset) {
+            offset = midpoint;
+          }
+
+          if (i === 0) {
+            textPositions[i] += 25;
+          } else {
+            do {
+              var dist = textPositions[i] + offset - textPositions[i - 1];
+              console.log(dist);
+              offset ++;
+            } while (dist < 25 && offset < 100);
+            textPositions[i] += offset;
+          }
+          offsets.push(offset);
+          return 'translate(' + offset + ', -15) rotate(-45) ';
+          // return 'translate(20, 40) rotate(45)'
+        });
+
+      elSelection
+        .append('line')
+        .attr('class', 'is-hideable')
+        .attr('x1', (d, i)=> {
+          return barRight(d, i) / 2;
+        })
+        .attr('x2', (d, i) => {
+          return 20 + offsets[i] - 25;
+        })
+        .attr('y1', 5)
+        .attr('y2',  -20)
+        .attr('stroke', (d, i) => {return colors(i)});
+    }
+
+    jobData.jobs.reverse();
+    var jobGroup = buildTypeTimeline(
+      jobData.jobs,
+      'job',
+      function(d,  i) {
+        return d.subTitles['Company Name'];
+      },
+      d3.scaleOrdinal(d3.schemeCategory20),
+      150
+    );
+
+    educationData.education.reverse();
+    var edGroup = buildTypeTimeline(
+      educationData.education,
+      'ed',
+      function(d,  i) {
+        return d.institution;
+      },
+      d3.scaleOrdinal(d3.schemeCategory20),
+      400
+    );
+
+
+
+
+    // var edList = chart.canvas.selectAll('.ed')
+    //   .data(educationData.education);
+
+    // edList.enter()
+    //   .append('rect')
+    //   .attr('class', 'ed')
+    //   .attr('x', (d) => {
+    //     return scaleX(d.startDate);
+    //   })
+    //   .attr('width', (d) => {
+    //     return scaleX(d.endDate) - scaleX(d.startDate);
+    //   })
+    //   .attr('y', (d, i)=> {
+    //     if (d.type === 'secondary') {
+    //       return 380;
+    //     } else {
+    //       return 370;
+    //     }
+    //   })
+    //   .attr('height', 10)
+    //   .on('mouseover', (d) => {
+    //     console.log(d.institution, d.subTitles['Dates attended or expected graduation']);
+    //   });
 
   }
 
